5566192cf18915322cd8f1b4c37621246b21c283
andrebarbosa24 <ruiandre1999@gmail.com>
2019-05-29 16:59:47 +0100

    oal

--- iapg_project/menu_remove.c
+++ iapg_project/menu_remove.c
@@ -3,3 +3,47 @@
 //
 
 #include "menu_remove.h"
+#include "funcoes.h"
+#include <stdio.h>
+
+
+int menu_remove() {
+    char opcao;
+    do {
+        printf("Escolha uma opcao entre as possiveis: \n");
+        printf(" [1] Remover Musica \n");
+        printf(" [2] Remover Artista \n");
+        printf(" [3] Voltar o Menu Principal \n");
+        printf(" [s] Sair\n");
+        fflush(stdin);
+        scanf("%c", &opcao);
+        switch (opcao) {
+            case '1':
+                printf("\n Insira o titulo \n");
+
+                break;
+            case '2':
+                printf("\n Insira o nome do Artista \n");
+
+                break;
+            case '3':
+
+
+                break;
+
+
+            case 's':
+            case 'S':
+                break;
+            default:
+                //system("cls"); // clear screen - cls windows or clear for linux/unix
+                printf(" Opcao invalida!!! \n");
+        }
+        if (opcao != 's' && opcao != 'S') {
+            printf("\n\nprima qualquer tecla para voltar ao menu...");
+            getchar();
+        }
+    } while (opcao != 's' && opcao != 'S');
+
+    return 0;
+}
